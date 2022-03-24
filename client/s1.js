5b92b70308b2ab3c3e28d85f9c21126a6e40842e
jnheo1216 <hjeionyng94@gmail.com>
2022-03-25 00:57:29 +0900

    0325 client/gsap on

--- client/s1.js
+++ client/s1.js
@@ -1,1 +1,14 @@
 console.log("hrello world");
+
+gsap.registerPlugin(ScrollTrigger); // 스크롤 플러그인 안정화
+gsap.utils.toArray("section").forEach((section, i) => {
+  ScrollTrigger.create({
+    trigger: section,
+    start: "top top", 
+    pin: true,
+    pinSpacing: false,
+  });
+});
+ScrollTrigger.create({
+  snap: 1 / 2
+});
