3de456977b8b441b4c71ba861649c86f161fa534
jnheo1216 <hjeionyng94@gmail.com>
2022-04-27 23:54:15 +0900

    0427 client/api response

--- client/s1.js
+++ client/s1.js
@@ -1,3 +1,5 @@
+const { response } = require("express");
+
 console.log("hrello world");
 
 // gsap.registerPlugin(ScrollTrigger); // 스크롤 플러그인 안정화
@@ -12,7 +14,6 @@ console.log("hrello world");
 // ScrollTrigger.create({
 //   snap: 1 / 2
 // });
-
 
 window.addEventListener("wheel", function(e){
   e.preventDefault();
@@ -34,3 +35,20 @@ $(window).on("wheel", function(e) {
   var posTop =(page-1) * $(window).height();
   mHtml.animate({scrollTop : posTop});
 });
+
+/////////////////////////////////////////////
+
+function printName () {
+  const name = document.getElementById('name').value;
+  document.getElementById('nameresult').innerText = name;
+}
+
+function getNames() {
+  fetch('http://localhost:8080/api/users')
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      document.getElementById('names').innerText = JSON.stringify(data.users);
+    })
+  
+}
